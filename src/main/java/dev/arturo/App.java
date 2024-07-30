7ba853428607d31177e54042236b5f734de9c20c
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-30 23:38:51 +0200

    complete refacting of my whole code, just need tests

--- src/main/java/dev/arturo/App.java
+++ src/main/java/dev/arturo/App.java
@@ -1,6 +1,8 @@
 package dev.arturo;
 
 import dev.arturo.models.CorporalMass;
+import dev.arturo.models.ImcCalculator;
+import dev.arturo.models.ImcStatus;
 
 /**
  * Hello world!
@@ -14,8 +16,20 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-  
         
+    CorporalMass corporalMass = new CorporalMass(88.0, 1.85);
+    
+    ImcCalculator imcCalculator = new ImcCalculator();
+
+    double savingWeight = imcCalculator.imcCalc(corporalMass);
+
+    ImcStatus imcStatus = new ImcStatus();
+
+    String savingStatus = imcStatus.getCorporalStatus(savingWeight);
+
+    System.out.println("Your weight is: " + savingWeight + "and you have a state of: " + savingStatus);
+
+
       
        
     }    
